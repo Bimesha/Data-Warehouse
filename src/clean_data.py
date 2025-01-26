533517f699251aae37c5a8b456590bd2884a960a
Bimesha <abimeshatharukshiperera9727@gmail.com>
2025-01-26 20:46:37 +0530

    chore: specify datasets to clean

--- src/clean_data.py
+++ src/clean_data.py
@@ -8,7 +8,16 @@ CLEANED_DATA_DIR = './data/cleaned/'
 # Ensure the cleaned data directory exists
 os.makedirs(CLEANED_DATA_DIR, exist_ok=True)
 
-
+# Define datasets to clean
+datasets = {
+    "olist_orders_dataset.csv": "orders",
+    "olist_customers_dataset.csv": "customers",
+    "olist_products_dataset.csv": "products",
+    "olist_order_items_dataset.csv": "order_items",
+    "olist_sellers_dataset.csv": "sellers",
+    "olist_order_payments_dataset.csv": "payments",
+    "olist_order_reviews_dataset.csv": "reviews",
+}
 
 
 
