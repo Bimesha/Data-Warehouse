388141ad5fcc4f2a94f06c99a64cf978a8d2ca12
Bimesha <abimeshatharukshiperera9727@gmail.com>
2025-01-26 20:47:27 +0530

    feat: add cleaning functions for each dataset

--- src/clean_data.py
+++ src/clean_data.py
@@ -19,7 +19,41 @@ datasets = {
     "olist_order_reviews_dataset.csv": "reviews",
 }
 
+# Cleaning functions for each dataset
+def clean_orders(df):
+    df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
+    df['order_approved_at'] = pd.to_datetime(df['order_approved_at'])
+    df['order_delivered_carrier_date'] = pd.to_datetime(df['order_delivered_carrier_date'])
+    df['order_delivered_customer_date'] = pd.to_datetime(df['order_delivered_customer_date'])
+    df['order_estimated_delivery_date'] = pd.to_datetime(df['order_estimated_delivery_date'])
+    df.drop_duplicates(subset=['order_id'], inplace=True)
+    return df
 
+def clean_customers(df):
+    df.drop_duplicates(subset=['customer_id'], inplace=True)
+    return df
+
+def clean_products(df):
+    df.drop_duplicates(subset=['product_id'], inplace=True)
+    return df
+
+def clean_order_items(df):
+    df.drop_duplicates(subset=['order_id', 'order_item_id'], inplace=True)
+    return df
+
+def clean_sellers(df):
+    df.drop_duplicates(subset=['seller_id'], inplace=True)
+    return df
+
+def clean_payments(df):
+    df.drop_duplicates(subset=['order_id', 'payment_sequential'], inplace=True)
+    return df
+
+def clean_reviews(df):
+    df['review_creation_date'] = pd.to_datetime(df['review_creation_date'])
+    df['review_answer_timestamp'] = pd.to_datetime(df['review_answer_timestamp'])
+    df.drop_duplicates(subset=['review_id'], inplace=True)
+    return df
 
 
 
